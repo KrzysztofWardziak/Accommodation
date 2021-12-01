bfb186a306ac8bcb09c14877b4fadf5fbd70127a
Krzysztof Wardziak <krzysztof.wardziak@oke.pl>
2021-12-01 17:31:45 +0100

    initial commit

---
+++ AccommodationWebApi/Controllers/AccommodationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccommodationWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccommodationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
